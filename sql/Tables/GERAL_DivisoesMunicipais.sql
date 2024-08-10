ac370dc4b735c45d7f774988af02ac7e8e9f3d46
Kenzo <79611160+Kenzo-Sugai@users.noreply.github.com>
2024-08-10 15:23:32 -0300

    Create GERAL_DivisoesMunicipais.sql

---
+++ sql/Tables/GERAL_DivisoesMunicipais.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [Geral].[DivisoesMunicipais](
+	[CD_MUNICIPIO] [int] NULL,
+	[NM_MUNICIPIO] [varchar](50) NULL,
+	[LATITUDE] [varchar](50) NULL,
+	[LONGITUDE] [varchar](50) NULL
+) ON [PRIMARY]
