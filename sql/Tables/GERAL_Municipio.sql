0db9f4756d1965b3d0f71df466d8b811be33713d
Kenzo <79611160+Kenzo-Sugai@users.noreply.github.com>
2024-08-10 15:24:29 -0300

    Create GERAL_Municipio.sql

---
+++ sql/Tables/GERAL_Municipio.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [Geral].[MUNICIPIO](
+	[UF] [int] NULL,
+	[NM_UF] [nvarchar](50) NULL,
+	[CD_MUNICIPIO] [int] NULL,
+	[NM_MUNICIPIO] [nvarchar](50) NULL,
+	[LATITUDE] [decimal](20, 3) NULL,
+	[LONGITUDE] [decimal](20, 3) NULL
+) ON [PRIMARY]
